74bfef5515716bbaa94bdb8fdf730226937f6f3e
paniHraskova <pekova.cikova@gmail.com>
2020-10-22 13:02:51 +0200

    adding Crc files for Msti35per

--- scripts10x/cellranger_mkref_Msti_1_2.sh
+++ scripts10x/cellranger_mkref_Msti_1_2.sh
@@ -3,4 +3,4 @@
 # use after logging in to Fritzen
 
 cd /home/s9/dthiel/petra/Msti
-cellranger mkref --genome=Msti_refTranscriptome --fasta=/home/s9/dthiel/petra/Msti/Mstichopi_reference_transcriptome_Tsai.fa --genes=/home/s9/dthiel/petra/Msti/MstiGTF_output.fa
+cellranger mkref --genome=Msti_refTranscriptome --fasta=/home/s9/dthiel/petra/Msti/Mstichopi_reference_transcriptome_Tsai.fa --genes=/home/s9/dthiel/petra/Msti/MstiGTF_output.fa --nthreads=
